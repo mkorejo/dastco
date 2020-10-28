2144190ed5fcd4ee66863ad641ca8cfb4dc9b259
Murad Korejo <mkorejo@gmail.com>
2020-10-28 15:44:26 -0500

    add burp common

---
+++ pkg/burpee/common.go
@@ -0,0 +1,31 @@
+package burpee
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// InvokeBurpSuiteEntAPI encapsulates the HTTP request/response flow and returns an HTTP response body.
+func InvokeBurpSuiteEntAPI(client *http.Client, request *http.Request) []byte {
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Error("Error sending the HTTP request: ", err)
+		return nil
+	}
+	log.Info("Response code: ", resp.StatusCode)
+
+	// Close resp.Body after reading
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	// Read the HTTP response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("Error reading the HTTP response: ", err)
+		return nil
+	}
+	return body
+}
